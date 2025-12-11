e28bae2f25b4c75f8de18c93297a38566d6d23e9
Rutger Pronk <rutger.pronk11@gmail.com>
2025-12-11 14:55:22 +0100

    Remove variables and also tofu apply new folder

--- 2-metallb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ip_addresses" {
-  description = "List of IP address ranges for MetalLB to allocate (e.g., ['192.168.1.230-192.168.1.250'])"
-  type        = list(string)
-}
-
-variable "traefik_ip" {
-  description = "Static IP address to assign to the Traefik LoadBalancer service"
-  type        = string
-}
