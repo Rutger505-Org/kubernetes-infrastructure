5e6e12df42465e9c80964a36e82964071faaf4d4
Rutger <119070855+Rutger505@users.noreply.github.com>
2026-09-25 11:41:35 +0200

    refactor(traefik): own the HTTP/3 UDP service instead of patching helm (#53)
    * refactor(traefik): own the http3 udp service instead of patching helm
    
    * chore: restore metallb-config provider lock file

--- 6-traefik-http3/variables.tf
+++ 6-traefik-http3/variables.tf
@@ -1,16 +1,16 @@
 variable "traefik_ip" {
-  description = "Static IP address of the Traefik LoadBalancer service (must match the MetalLB config)"
+  description = "Static IP address shared by the traefik and traefik-http3 LoadBalancer services (must come from the MetalLB pool in 3-metallb-config)"
   type        = string
 }
 
 variable "advertised_port" {
-  description = "Port advertised in the Alt-Svc header, i.e. the public UDP port clients reach"
+  description = "Public UDP port for QUIC, also the port advertised in the Alt-Svc header"
   type        = number
   default     = 443
 }
 
 variable "websecure_container_port" {
-  description = "Numeric container port of Traefik's websecure entrypoint, used as targetPort for the UDP/HTTP3 service port"
+  description = "Numeric container port of Traefik's websecure entrypoint, used as the targetPort of the UDP service"
   type        = number
   default     = 8443
 }
